33f171cff0c83140ff90fa1c1a3b0a9fa6d3f239
kuritaseiya <newseiya0222@icloud.com>
2020-10-30 01:49:13 +0900

    Basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'popopo' && password == '0222'
+    end
+  end
 end
