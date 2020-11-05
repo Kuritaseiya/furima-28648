a226b6d401eb801416f9cbe3e90eb176829a9d62
kuritaseiya <newseiya0222@icloud.com>
2020-11-05 21:55:39 +0900

    userテーブル編集

--- db/migrate/20201105115939_create_users.rb
+++ db/migrate/20201105115939_create_users.rb
@@ -3,6 +3,14 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
 
       t.timestamps
+      t.string :nickname,           null :false
+      t.string :encrypted_password, null :false
+      t.string :email,              null :false
+      t.string :name,               null :false
+      t.string :last_name,          null :false
+      t.string :KANA_name,          null :false
+      t.string :KANA_last_name,     null :false
+      t.string :birthday,           null :false
     end
   end
 end
