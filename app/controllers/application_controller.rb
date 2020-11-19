c664d8152967616a7d7eed2ecfe3c3db4b87841a
kuritaseiya <newseiya0222@icloud.com>
2020-11-19 18:07:52 +0900

    ユーザーバリデーション修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def configure_permitted_parameters  # メソッド名は慣習
+    # deviseのUserモデルにパラメーターを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:name,:kana_last_name,:kana_name,:birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["@@@@"] && password == ENV["~~~~"]
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,26 @@ class User < ApplicationRecord
   has_many :items
   has_many :buys
 
-  validates :nickname, presence: true 
-  validates :email, presence: true 
   validates :email, uniqueness: { case_sensitive: false }
-  validates :email, inclusion: {in: %w(@)} 
-  validates :encrypted_password, length: {minimum: 6} 
-  validates :encrypted_password, inclusion: { in: %w(/\A[a-z A-Z 0-9]\z/) }
-  validates :name, inclusion: { in: %w(/\A(?:\p{Hiragana}|\p{Katakana}|[--]|[--龠々])+\z/)}
-  validates :last_name, inclusion: { in: %w(/\A(?:\p{Hiragana}|\p{Katakana}|[--]|[--龠々])+\z/)}
-  validates :KANA_name, inclusion: { in: %w(/\A[ｧ-ﾝﾞﾟ]+\z/)}
-  validates :KANA_last_name, inclusion: { in: %w(/\A[ｧ-ﾝﾞﾟ]+\z/)}
+  validates :email, presence: true
+  validates :email, format: { with:/@/}
+  validates :password, length: {minimum: 6} 
+  validates :password, format: { with: /\A[a-z A-Z 0-9]+\z/ }
+  validates :password, presence: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: 'Full-width characters' } do
+    validates :name
+    validates :last_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/}do
+    validates :kana_name
+    validates :kana_last_name
+  end
+
 end
