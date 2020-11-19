ba84e5f6039de4aeacf11256b335e09a3e482529
kuritaseiya <newseiya0222@icloud.com>
2020-11-19 13:07:41 +0900

    usersテーブル修正

--- db/migrate/20201105115939_create_users.rb
+++ db/migrate/20201105115939_create_users.rb
@@ -4,14 +4,13 @@ class CreateUsers < ActiveRecord::Migration[6.0]
 
       t.timestamps
       t.string :nickname,           null: false
-      t.string :encrypted_password, null: false
-      t.string :encrypted_password2, null: false
-      t.string :email,              null: false
+      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ""
       t.string :name,               null: false
       t.string :last_name,          null: false
-      t.string :KANA_name,          null: false
-      t.string :KANA_last_name,     null: false
-      t.string :birthday,           null: false
+      t.string :kana_name,          null: false
+      t.string :kana_last_name,     null: false
+      t.date :birthday,           null: false
     end
     #add_index :users, :email,                unique: true
     #add_index :users, :reset_password_token, unique: true
